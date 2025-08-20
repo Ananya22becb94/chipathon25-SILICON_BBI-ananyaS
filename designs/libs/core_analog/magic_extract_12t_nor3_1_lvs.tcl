c670c695655096a59d484d2479ed9c1f9677c664
Ananya22becb94 <satapathyananya08@gmail.com>
2025-08-20 16:42:18 +0200

    addedn5 lvs.tcl files

---
+++ designs/libs/core_analog/magic_extract_12t_nor3_1_lvs.tcl
@@ -0,0 +1,17 @@
+# Load the desired technology
+#tech load /foss/pdks/gf180mcuD/libs.tech/magic/gf180mcuD.tech 
+
+# Load your layout cell
+load /foss/designs/work/cellwise/12T/nor3/gf180mcu_osu_sc_gp12t3v3__nor3_1.mag
+
+# Extract the layout
+extract all
+
+# Optional: Set extraction parameters
+#ext2spice hierarchy on         ;# Keeps subcircuit hierarchy (or 'off' for flat netlist)
+ext2spice scale off            ;# Use real unit scaling
+ext2spice cthresh 100f        ;# Extract all capacitances
+
+# Output the SPICE file, in ngspice format, with a chosen output filename
+ext2spice -d -o lvs/gf180mcu_osu_sc_gp12t3v3__nor3_1_ext.spice -f ngspice
+
